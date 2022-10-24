b90a9495c15322ec7c4aac1e6db7eb3505fdc5e7
MarcosFernandesF <marcosrff15@gmail.com>
2022-10-24 15:54:24 -0300

    Aula 04: Iterando em listas
    - Percorrendo Listas com While e For
    
    - Textos dinâmicos
    
    - Função com parâmentros

--- js-web/main.js
+++ js-web/main.js
@@ -1,7 +1,38 @@
-function tocaSom () {
-    document.querySelector("#som_tecla_pom").play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTeclas[0].onclick = tocaSom;
+/*let contador = 0;
+
+Exemplo com while
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    // Função anonima e utilizada para não realizar a função tocaSom na hora em que o while e feito
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+    contador += 1
+}*/
+
+//Exemplo com for
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    // Função anonima e utilizada para não realizar a função tocaSom na hora em que o while e feito
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+}
